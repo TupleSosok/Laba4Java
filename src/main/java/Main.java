0d78438aec250c486661e376904fa1995caded7d
TupleSosok <jokylionplay@gmail.com>
2024-05-27 20:32:51 +0300

    Работает, без исключений

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,11 @@
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.List;
 
-import static java.io.File.separator;
-
 public class Main {
-
-    public static List<Employee> getListFromCSVFile(String filename) throws IOException {
-
-        InputStream in = Main.class.getClassLoader().getResourceAsStream("src/main/resources/" + filename);
-        CSVReader reader = new CSVReader(new InputStreamReader(in));
-        CsvToBean<Employee> csvToBean = new CsvToBeanBuilder(reader)
-                .withSkipLines(1)
-                .withSeparator(';')
-                .withType(Employee.class)
-                .build();
-        return csvToBean.parse();
-    }
     public static void main(String[] args) throws IOException {
-        List<Employee> employees = getListFromCSVFile("foreign_names.csv");
+        List<Employee> employees = CSVReader.getListFromCSVFile("foreign_names.csv");
         for(Employee i : employees){
             System.out.println(i.toString());
         }
-
     }
 }
