4b4fbbea94f49340254ce95b96a86ece0af5c724
TupleSosok <jokylionplay@gmail.com>
2024-05-27 16:21:35 +0300

    lombok не рабочий варинат

---
+++ src/main/java/Employee.java
@@ -0,0 +1,19 @@
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Employee {
+
+    Long id;
+
+    String name;
+
+    String male;
+
+    Date birthdate;
+
+    String division;
+
+    int salary;
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.io.File.separator;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static List<Employee> getListFromCSVFile(String filename) throws IOException {
+
+        InputStream in = Main.class.getClassLoader().getResourceAsStream("src/main/resources/" + filename);
+        CSVReader reader = new CSVReader(new InputStreamReader(in));
+        CsvToBean<Employee> csvToBean = new CsvToBeanBuilder(reader)
+                .withSkipLines(1)
+                .withSeparator(';')
+                .withType(Employee.class)
+                .build();
+        return csvToBean.parse();
+    }
+    public static void main(String[] args) throws IOException {
+        List<Employee> employees = getListFromCSVFile("foreign_names.csv");
+        for(Employee i : employees){
+            System.out.println(i.toString());
+        }
 
     }
 }
