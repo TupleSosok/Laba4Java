4230bed53cbf39a658f1a56ba68fb672f39e7d90
TupleSosok <jokylionplay@gmail.com>
2024-05-27 20:54:11 +0300

    С анатациями для связи с файлом

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,5 @@
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
 import lombok.Data;
 
 import java.util.Date;
@@ -5,15 +7,22 @@ import java.util.Date;
 @Data
 public class Employee {
 
+    @CsvBindByName(column = "id")
     Long id;
 
+    @CsvBindByName(column = "name")
     String name;
 
+    @CsvBindByName(column = "gender")
     String male;
 
+    @CsvBindByName(column = "BirtDate")
+    @CsvDate("dd.MM.yyyy")
     Date birthdate;
 
+    @CsvBindByName(column = "Division")
     String division;
 
+    @CsvBindByName(column = "Salary")
     int salary;
 }
